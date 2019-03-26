e7264234384640c2325c6a83911bdaf50dd902e8
ddot727 <ddot727@gmail.com>
2019-03-26 17:23:01 -0400

    created update by id action

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const knexConfig = {
   useNullAsDefault: true,
   connection: {
     filename: "./data/lambda.sqlite3"
-  },
-  debug: true
+  }
+  // debug: true
 };
 
 const db = knex(knexConfig);
@@ -51,6 +51,26 @@ server.get("/api/zoos/:id", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Some useful error message"
+    });
+  }
+});
+
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const count = await db("zoos")
+      .where({ id })
+      .update(req.body);
+    if (count > 0) {
+      res.status(200).json(count);
+    } else {
+      res.status(404).json({
+        message: "Some useful 404 message"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Some useful Error message"
     });
   }
 });
